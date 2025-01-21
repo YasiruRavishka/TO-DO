ec0b2bce5332e96a28f0dd91f15eaf18cca2d301
YasiruRavishka <YasiruRavishka47@gmail.com>
2025-01-21 11:16:14 +0530

    app themes

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kLightTheme = Brightness.light;
+const kLightPrimarySwatch = Colors.blue;
+
+const kDarkTheme = Brightness.dark;
+const kDarkPrimarySwatch = Colors.blue;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/core/app_colors.dart';
 import 'package:to_do/features/home/home_page.dart';
+import 'package:to_do/features/profile/profile_page/profile_page.dart';
 
 import 'core/navigator_keys.dart';
 
@@ -15,6 +17,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'To-Do',
       navigatorKey: globalNavigatorKey,
+      theme: ThemeData(
+        brightness: kLightTheme,
+        primarySwatch: kLightPrimarySwatch,
+      ),
+      darkTheme: ThemeData(
+        brightness: kDarkTheme,
+        primarySwatch: kDarkPrimarySwatch,
+      ),
       home: const HomePage(),
     );
   }
